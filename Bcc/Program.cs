8cdf3e0b4e01e4265c34632789e8ab8bb8ef7a4b
bidemi14 <bidemielelu05@gmail.com>
2025-08-29 21:28:45 +0100

    changes the name of the file to Program. cs , make the code top level(removed the class)

---
+++ Bcc/Program.cs
@@ -0,0 +1,49 @@
+using System.Text.RegularExpressions;
+
+if (args.Length != 2 )
+{
+    Console.WriteLine("Usage: ccwc -c|-l|-w|-m <filename>");
+    return;
+}
+    
+string option = args[0];
+string filename = args[1];
+
+try
+{
+    if(option == "-c") 
+    {
+        byte[] fileBytes;
+        fileBytes = File.ReadAllBytes(filename);
+        Console.WriteLine(fileBytes.Length);
+                    
+    }
+    else if (option == "-l")
+    {
+        string[] lines = File.ReadAllLines(filename);
+        Console.WriteLine(lines.Length);
+    }
+                
+    else if (option == "-w")//step 3
+    {
+        string text = File.ReadAllText(filename);
+        text=Regex.Replace(text, @"\s+", " ");
+        int words = 0;
+        foreach (string word in text.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+        {
+            words++; 
+        }
+        Console.WriteLine(words);
+    }
+                
+    else if (option == "-m")//step 4 
+    {
+        string text = File.ReadAllText(filename);
+        Console.WriteLine(text.Length);
+    }
+                
+}
+catch (FileNotFoundException)
+{
+    Console.WriteLine($"Error: File '{filename}' not found.");
+}
